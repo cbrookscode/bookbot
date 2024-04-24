1ee8f50f44b09d904e88a0618451ca24abebbdb2
cbrookscode <colbybrooks@myyahoo.com>
2024-04-24 15:40:26 -0400

    updated main code for bookbot so that it incluides a dictionary with the counts of each character in the book. Also cleaned up ordering of the defined functions to make more intuitive sense for how they are ordered based on code objectives.

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    chars_dict = get_char_dict(text)
     print(f"{num_words} words found in the document")
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+    print(chars_dict)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_char_dict(text):
+    chars = {}
+    lower_case_words = text.lower()
+    for char in lower_case_words:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
 
 main()
